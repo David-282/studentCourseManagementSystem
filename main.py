6dc65d32a96531ce40142d7b0000faed415a9b5f
David-282 <afolabidavid94@gmail.com>
2026-04-07 11:31:51 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from routes import student_router, facilitator_router
+
+app = FastAPI()
+
+
+app.include_router(student_router.router)
+app.include_router(facilitator_router.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port=8000)
